7c31dcad3c463ac95069c053b326aa68e37455db
jjjumper <halliho@yahoo.com>
2014-09-02 16:27:22 -0400

    lolol

---
+++ magic 8 ball.py
@@ -0,0 +1,23 @@
+import random
+
+
+
+
+
+print('welcome to magic 8 ball')
+print('muuuuuuhahahaha')
+
+
+
+answers = ['YES!','NO!!!','MAYBE?','you are not cool','lololooolllllllllloooooo
+           ]
+
+
+
+while True:
+    print('ask me a question')
+  qestion = input('>')
+else:
+elif
+    answer = random.choice(answers)
+    print(answer)
